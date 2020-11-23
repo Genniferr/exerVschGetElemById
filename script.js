b7b623cbdbecfe51dedc8e1671e1330018327747
Genniferr <u6020155@utah.edu>
2020-11-22 19:33:26 -0700

    Get Element

---
+++ script.js
@@ -0,0 +1,7 @@
+//document.getElementById("text").textContent = "Some Text"
+//let text = document.getElementById('divA').textContent;
+//document.getElementById('divA').textContent = 'This text is different!';
+
+var element = document.getElementById("done")
+element.textContent = "This is a JavaScript script"
+console.log("Assignment complete")
